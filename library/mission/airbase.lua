c92b995da3cb9be003b43203d69e5528f72b047a
omltcat <6239696+omltcat@users.noreply.github.com>
2026-01-26 20:11:25 +0800

    Add warehouse class

--- library/mission/airbase.lua
+++ library/mission/airbase.lua
@@ -14,3 +14,8 @@ Airbase.Category = {
 ---In the case of airbases, the callsign of world airbases is defined by the game. Callsigns for units, farps, or ships can be specified by the user with the mission editor or scripting engine.
 ---@return string
 function Airbase:getCallsign() end
+
+---Returns the warehouse object associated with the airbase object.<br>
+---Can then be used to call the warehouse class functions to modify the contents of the warehouse. 
+---@return Warehouse
+function Airbase:getWarehouse() end
